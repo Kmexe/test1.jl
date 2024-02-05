2a8689c1127e2e526ec4bf8a838f39c22894e3ce
Kmexe <161409976@qq.com>
2024-02-05 17:31:57 +0800

    init docs

---
+++ docs/build/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html","page":"Home","title":"Home","text":"CurrentModule = test1","category":"page"},{"location":"index.html#test1","page":"Home","title":"test1","text":"","category":"section"},{"location":"index.html","page":"Home","title":"Home","text":"Documentation for test1.","category":"page"},{"location":"index.html","page":"Home","title":"Home","text":"","category":"page"},{"location":"index.html","page":"Home","title":"Home","text":"Modules = [test1]","category":"page"}]
+}
