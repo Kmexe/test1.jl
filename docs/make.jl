193a0bdf380b8d28928ed71344217093014cebe8
Kmexe <161409976@qq.com>
2024-02-05 18:39:44 +0800

    make.jl

---
+++ docs/make.jl
@@ -0,0 +1,23 @@
+using test1
+using Documenter
+
+DocMeta.setdocmeta!(test1, :DocTestSetup, :(using test1); recursive=true)
+
+makedocs(;
+    modules=[test1],
+    authors="Kmexe",
+    sitename="test1.jl",
+    format=Documenter.HTML(;
+        canonical="https://Kmexe.github.io/test1.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/Kmexe/test1.jl",
+    devbranch="main",
+)
